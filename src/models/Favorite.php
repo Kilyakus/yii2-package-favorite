accd4c41af207415818d43f200b5b860ad8346f7
Kilyakus <aleksandr.kilyakov@gmail.com>
2021-04-01 21:33:44 +0500

    initial commit

--- src/models/Favorite.php
+++ src/models/Favorite.php
@@ -10,116 +10,116 @@ use kilyakus\modules\behaviors\SortableModel;
 
 class Favorite extends ActiveRecord
 {
-    const STATUS_OFF = 0;
-    const STATUS_ON = 1;
+	const STATUS_OFF = 0;
+	const STATUS_ON = 1;
 
-    public static function tableName()
-    {
-        return 'favorites';
-    }
+	public static function tableName()
+	{
+		return 'favorites';
+	}
 
-    public function rules()
-    {
-        return [
-            [['class', 'title_track', 'title_untrack'], 'required'],
-            [['item_id', 'status'], 'integer'],
-            [['class', 'name'], 'string', 'max' => 255],
-            [['title_track','title_untrack'], 'trim'],
-        ];
-    }
+	public function rules()
+	{
+		return [
+			[['class', 'title_track', 'title_untrack'], 'required'],
+			[['item_id', 'status'], 'integer'],
+			[['class', 'name'], 'string', 'max' => 255],
+			[['title_track','title_untrack'], 'trim'],
+		];
+	}
 
-    public function behaviors()
-    {
-        return [
-            SortableModel::className()
-        ];
-    }
+	public function behaviors()
+	{
+		return [
+			SortableModel::className()
+		];
+	}
 
-    public function getTitle($status)
-    {
-        if($status){
-            return Yii::t('easyii', $this->title_untrack);
-        }else{
-            return Yii::t('easyii', $this->title_track);
-        }
-    }
+	public function getTitle($status)
+	{
+		if($status){
+			return Yii::t('easyii', $this->title_untrack);
+		}else{
+			return Yii::t('easyii', $this->title_track);
+		}
+	}
 
-    public function relative($model, $owner, $id = null)
-    {
-        $query = [
-            'and',
-            ['class' => get_class($model)],
-            ['item_id' => $model->primaryKey],
-            ['owner_class' => get_class($owner)],
-            ['owner_id' => $owner->primaryKey],
-            ['favorite_id' => $id],
-        ];
+	public function relative($model, $owner, $id = null)
+	{
+		$query = [
+			'and',
+			['class' => get_class($model)],
+			['item_id' => $model->primaryKey],
+			['owner_class' => get_class($owner)],
+			['owner_id' => $owner->primaryKey],
+			['favorite_id' => $id],
+		];
 
-        $searchModel  = \Yii::createObject(FavoriteAssign::className());
-        $dataProvider = $searchModel->search(\Yii::$app->request->get());
-        $dataProvider->query->andFilterWhere($query);
+		$searchModel  = \Yii::createObject(FavoriteAssign::className());
+		$dataProvider = $searchModel->search(\Yii::$app->request->get());
+		$dataProvider->query->andFilterWhere($query);
 
-        return $dataProvider->query;
-    }
+		return $dataProvider->query;
+	}
 
-    public function list($model, $owner, $id = null)
-    {
-        $query = [
-            'and',
-            ['class' => get_class($model)],
-            ['item_id' => $id],
-            ['owner_class' => get_class($owner)],
-            ['owner_id' => $owner->primaryKey],
-        ];
+	public function list($model, $owner, $id = null)
+	{
+		$query = [
+			'and',
+			['class' => get_class($model)],
+			['item_id' => $id],
+			['owner_class' => get_class($owner)],
+			['owner_id' => $owner->primaryKey],
+		];
 
-        $searchModel  = \Yii::createObject(FavoriteAssign::className());
-        $dataProvider = $searchModel->search(\Yii::$app->request->get());
-        $dataProvider->query->andFilterWhere($query);
+		$searchModel  = \Yii::createObject(FavoriteAssign::className());
+		$dataProvider = $searchModel->search(\Yii::$app->request->get());
+		$dataProvider->query->andFilterWhere($query);
 
-        return $dataProvider;
-    }
+		return $dataProvider;
+	}
 
-    public function items($model, $owner, $options = [])
-    {
-        $className = get_class($model);
+	public function items($model, $owner, $options = [])
+	{
+		$className = get_class($model);
 
-        $list = self::list($model, $owner);
+		$list = self::list($model, $owner);
 
-        if(!empty($options['where'])){
-            $list->query->andFilterWhere($options['where']);
-        }
-        if(!empty($options['orderBy'])){
-            $list->query->orderBy($options['orderBy']);
-        }
+		if(!empty($options['where'])){
+			$list->query->andFilterWhere($options['where']);
+		}
+		if(!empty($options['orderBy'])){
+			$list->query->orderBy($options['orderBy']);
+		}
 
-        $links = ArrayHelper::getColumn($list->query->all(), 'item_id');
+		$links = ArrayHelper::getColumn($list->query->all(), 'item_id');
 
-        $searchModel  = \Yii::createObject($className);
-        $dataProvider = $searchModel->search(\Yii::$app->request->get());
-        $dataProvider->query->where([$className::primaryKey()[0] => $links]);
+		$searchModel  = \Yii::createObject($className);
+		$dataProvider = $searchModel->search(\Yii::$app->request->get());
+		$dataProvider->query->where([$className::primaryKey()[0] => $links]);
 
-        return $dataProvider;
-    }
+		return $dataProvider;
+	}
 
-    public function search($params)
-    {
-        $query = static::find();
+	public function search($params)
+	{
+		$query = static::find();
 
-        $dataProvider = new ActiveDataProvider(['query' => $query]);
-        $dataProvider->sort->defaultOrder = ['status' => SORT_ASC, 'order_num' => SORT_ASC];
-        $dataProvider->pagination->pageSize = Yii::$app->session->get('per-page', 20);
+		$dataProvider = new ActiveDataProvider(['query' => $query]);
+		$dataProvider->sort->defaultOrder = ['status' => SORT_ASC, 'order_num' => SORT_ASC];
+		$dataProvider->pagination->pageSize = Yii::$app->session->get('per-page', 20);
 
-        if (!($this->load($params) && $this->validate())) {
-            return $dataProvider;
-        }
+		if (!($this->load($params) && $this->validate())) {
+			return $dataProvider;
+		}
 
-        $query
-            ->andFilterWhere(['class' => $this->class])
-            ->andFilterWhere(['item_id' => $this->item_id])
-            ->andFilterWhere(['like', 'title', $this->title_track])
-            ->andFilterWhere(['like', 'title', $this->title_untrack])
-            ->andFilterWhere(['status' => $this->status]);
+		$query
+			->andFilterWhere(['class' => $this->class])
+			->andFilterWhere(['item_id' => $this->item_id])
+			->andFilterWhere(['like', 'title', $this->title_track])
+			->andFilterWhere(['like', 'title', $this->title_untrack])
+			->andFilterWhere(['status' => $this->status]);
 
-        return $dataProvider;
-    }
+		return $dataProvider;
+	}
 }
